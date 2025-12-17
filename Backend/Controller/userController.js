1d2cd20da118e531c8992250eea2dbec1832d5ab
Vandan-Bhangale <vandan9804@gmail.com>
2025-12-17 11:52:47 +0530

    Created user model and register controller

--- Backend/Controller/userController.js
+++ Backend/Controller/userController.js
@@ -1,3 +1,31 @@
+const bcrypt = require("bcrypt");
+const userModel = require("../Models/userModel");
+
+exports.postRegister = async (req,res) => {
+    const { name,email,password } = req.body;
+
+    try{
+        const hashedPassword = await bcrypt.hash(password,12);
+
+        const newUser = new userModel ({
+            name,
+            email,
+            password:hashedPassword
+        });
+
+        await newUser.save();
+
+        res.status(200).json({message:"User saved successfully"});
+    } catch(err) {
+
+        //Checking for duplicate emails.
+        if(err.code === 11000) {
+           return res.status(400).json({message:"User already exists."});
+        }
+        res.status(500).json({message:"Internal server error."});
+    }
+}
+
 exports.postLogin = (req,res) => {
     res.send("Login page");
 }
--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const userRoute = require("./Routes/userRoute");
 
 const app = express();
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+const URI = process.env.MONGODB_URI;
 
 const PORT = process.env.PORT || 3001;
 
 app.use("/api",userRoute);
 
-app.listen(PORT,() => {
-    console.log(`Server is now live on http://localhost:${PORT}`);
+mongoose.connect(URI).
+then(() => {
+    app.listen(PORT,() => {
+        console.log('Connectiong to DB is successfull.');
+        console.log(`Server is now live on http://localhost:${PORT}`);
+    });
+}).catch ((err) => {
+    console.log("Error while connecting to DB.",err);
 })

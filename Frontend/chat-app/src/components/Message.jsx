149272c9360fd2ca62c220a4b63b9a7149ffdde2
Vandan-Bhangale <vandan9804@gmail.com>
2025-12-30 16:21:14 +0530

    Made Message sent and recieving functionality

--- Frontend/chat-app/src/components/Message.jsx
+++ Frontend/chat-app/src/components/Message.jsx
@@ -1,21 +1,33 @@
-function Message() {
+import { useContext } from "react";
+import { AuthContext } from "../Context/authContext";
+import useConversation from "../Store/useConversation";
+
+function Message({ message }) {
+  const { user } = useContext(AuthContext);
+  const { selectedConversation } = useConversation();
+  const fromMe = message.senderId === user._id;
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
+  const profilePic = fromMe ? user.profilePic : selectedConversation?.profilePic;
+  const bubbleColor = fromMe ? "bg-blue-500" : "";
+
   return (
     <>
-      <div className="chat chat-start mb-1.5">
+      <div className={`chat mb-1.5 ${chatClassName}`}>
         {/* Avatar */}
         <div className="chat-image avatar">
           <div className="w-10 rounded-full ring ring-blue-200 ring-offset-2">
-            <img
-              alt="User avatar"
-              src="https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
-            />
+            <img alt="User avatar" src={profilePic} />
           </div>
         </div>
 
         {/* Message Bubble */}
-        <div className="chat-bubble shadow-md max-w-[70%]">
-          <p className="leading-relaxed">
-            It was said that you would destroy the Sith, not join them.
+        <div className={`chat-bubble ${bubbleColor} shadow-md max-w-[70%]`}>
+          <p className="leading-relaxed">{message.message}</p>
+          <p className="text-sm text-white">
+            {new Date(message.createdAt).toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
           </p>
         </div>
       </div>
--- Frontend/chat-app/src/components/Messages.jsx
+++ Frontend/chat-app/src/components/Messages.jsx
@@ -1,16 +1,44 @@
+import { useState } from "react";
 import Message from "./Message";
+import { useEffect } from "react";
+import useConversation from "../Store/useConversation";
+import axios from "axios";
 
 function Messages() {
-    return (
-        <>
-            <Message></Message>
-            <Message></Message>
-            <Message></Message>
-            <Message></Message>
-            <Message></Message>
-            <Message></Message>
-        </>
-    )
+  const [loading, setLoading] = useState(false);
+  const { messages, setMessages, selectedConversation } = useConversation();
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchMessages = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_GENERAL_API}/api/${selectedConversation._id}`,
+          { withCredentials: true }
+        );
+
+        console.log(response.data);
+        setMessages(response.data);
+      } catch (error) {
+        console.error("Error fetching messages: ", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    if (selectedConversation?._id) fetchMessages();
+  }, [selectedConversation]);
+
+  return (
+    <>
+      <div className="messages">
+        {messages.length === 0 && <p>No messages yet</p>}
+
+        {messages.map((message) => (
+          <Message key={message._id} message={message} />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Messages;

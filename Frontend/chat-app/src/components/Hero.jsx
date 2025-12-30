fb8b5807f221c4e0df46f09417e4d5c3882eb3a7
Vandan-Bhangale <vandan9804@gmail.com>
2025-12-30 23:47:04 +0530

    Improved functionality

--- Frontend/chat-app/src/components/Hero.jsx
+++ Frontend/chat-app/src/components/Hero.jsx
@@ -1,13 +1,19 @@
+import { useContext } from "react";
 import image1 from "../assets/Image1.webp";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../Context/authContext";
 
 function Hero() {
+  const { isAuthenticated } = useContext(AuthContext);
+
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-dvh text-center">
         <div className="headline">
           <h1 className="text-5xl font-bold">Your new way to</h1>
-          <h1 className=" mt-4 text-4xl font-bold bg-linear-to-r from-red-500 via-orange-400 to-yellow-400 bg-clip-text text-transparent">Communicate</h1>
+          <h1 className=" mt-4 text-4xl font-bold bg-linear-to-r from-red-500 via-orange-400 to-yellow-400 bg-clip-text text-transparent">
+            Communicate
+          </h1>
         </div>
 
         <div className="featureLine mt-6 opacity-50">
@@ -17,10 +23,28 @@ function Hero() {
           <p>Be free to speak in group chats.</p>
         </div>
 
-        <Link to="/register" className="btn btn-warning mt-8 border rounded-2xl h-10 w-30 font-bold text-white">Get Started</Link>
+        {isAuthenticated ? (
+          <Link
+            to="/chatPage"
+            className="btn btn-warning mt-8 border rounded-2xl h-10 w-30 font-bold text-white"
+          >
+            Go to Chats
+          </Link>
+        ) : (
+          <Link
+            to="/register"
+            className="btn btn-warning mt-8 border rounded-2xl h-10 w-30 font-bold text-white"
+          >
+            Get Started
+          </Link>
+        )}
 
         <div className="HeroImg mt-8 inline-block p-0.75 rounded-xl bg-linear-to-r from-orange-500 via-yellow-500 to-red-500">
-          <img src={image1} alt="Image" className="w-64 md:w-96 lg:w-125 border-none rounded-2xl"/>
+          <img
+            src={image1}
+            alt="Image"
+            className="w-64 md:w-96 lg:w-125 border-none rounded-2xl"
+          />
         </div>
       </div>
     </>

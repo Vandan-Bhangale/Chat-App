f05527680554ad45eedf0628cf6f225ebe6cc5ce
Vandan-Bhangale <vandan9804@gmail.com>
2026-01-02 11:08:49 +0530

    Updated socket.io using online user list

--- Backend/Socket/socket.js
+++ Backend/Socket/socket.js
@@ -1,11 +1,9 @@
-const {Server} = require("socket.io");
+const { Server } = require("socket.io");
 const express = require("express");
 const http = require("http");
 
 const app = express();
 const server = http.createServer(app);
-
-console.log("🔥 SOCKET SERVER FILE EXECUTED");
 
 const io = new Server(server, {
   cors: {
@@ -14,16 +12,35 @@ const io = new Server(server, {
   },
 });
 
-console.log("io instance created");
+const userSocketMap = {}; //(userId: [socketId1, socketId2, ...]);
 
 io.on("connection", (socket) => {
-  console.log("Scoket connection initiated");
+  console.log("Socket connection initiated");
   console.log("New client connected", socket.id);
+  const userId = socket.handshake.query.userId;
 
-    //Socket.on() is used to listen for events and can be used in both frontend & backend.
-  socket.on("disconnect",() => {
+  if (userId && userId !== 'undefined') {
+    if (!userSocketMap[userId]) {
+      userSocketMap[userId] = [];
+    }
+    userSocketMap[userId].push(socket.id);
+    socket.userId = userId;
+  }
+
+  io.emit("OnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
-  })
+    if (userId && userSocketMap[userId]) {
+      userSocketMap[userId] = userSocketMap[userId].filter(
+        (id) => id !== socket.id
+      );
+      if (userSocketMap[userId].length === 0) {
+        delete userSocketMap[userId];
+      }
+    }
+    io.emit("OnlineUsers", Object.keys(userSocketMap));
+  });
 });
 
 module.exports = { app, io, server };

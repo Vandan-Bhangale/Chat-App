9fe13cbc21b129164271266a8d6051f556ec6aae
Vandan-Bhangale <vandan9804@gmail.com>
2026-01-03 10:12:14 +0530

    Sending message to user in real time

--- Backend/Socket/socket.js
+++ Backend/Socket/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],      //Frontend URL
+    origin: ["https://chat-app-nine-zeta-56.vercel.app/"],      //Frontend URL
     credentials: true,
   },
 });
